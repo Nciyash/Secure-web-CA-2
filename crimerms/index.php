3d0b34c63c2c5e8464f9ff4b98cfcfb8b4008f2e
Nciyash <x23205415@student.ncirl.ie>
2024-12-06 11:48:21 +0000

    First Commit
    Uploading my previous project

---
+++ crimerms/index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Crime Record Management System | Home</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+</head>
+<body>
+<div class="header">
+	
+</div>
+<?php include_once('includes/header.php');?>
+
+<!--content-->
+	<div class="content">
+		<div class="container">
+			
+			<div class="content-welcome">
+			
+				<div class="col-md-8 come">
+					<div class=" welcome">
+						<a href="index.php"><img src="images/unnamed.png" alt=""></a>
+					</div>
+				</div>
+				<div class="col-md-4 red">
+				<h3>Mission & Vision</h3>
+				<h2> Mission: To Keep You, Your Family, and Our
+					Community Safe.
+				</h2>
+				<h2>
+					Vision: Respected & Trusted by Indians</h2>
+				
+				</div>
+				<div class="clearfix"> </div>
+		
+			</div>
+			<!---->
+		
+			<!---->
+		</div>
+		
+		
+</body>
+</html>
